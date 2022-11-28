985b3f29bc8bb33c3a219b40f86a25f29ba6e684
Rana-Tarawi <ranatarawi45@gmail.com>
2022-11-28 23:41:40 +0200

    second

---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class colortheme {
+  // static const Color mainBlue = Colors.orange;
+  static const Color lightPurple = Color.fromRGBO(126, 87, 194, 1);
+  static const Color blueGray = Color.fromRGBO(236, 239, 241, 1);
+  static const Color saimon = Color.fromARGB(255, 252, 120, 105);
+  static const Color black = Colors.black;
+  static const Color white = Color(0xFFEEEEEE);
+  static const Color gold = Color.fromARGB(255, 255, 208, 128);
+  static const Color green = Color.fromARGB(255, 95, 222, 101);
+  static const Color lightGray = Color.fromRGBO(224, 224, 224, 1);
+}
---
+++ lib/widgets/button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:graduation_project_app/colors.dart';
+
+Widget button({
+  required String text,
+  required VoidCallback onpress,
+  required double width,
+  required double height,
+}) =>
+    Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: colortheme.lightPurple,
+        borderRadius: BorderRadius.circular(height * 0.4),
+        boxShadow: [
+          BoxShadow(
+            color: colortheme.lightPurple.withOpacity(0.5),
+            blurRadius: 12,
+            offset: const Offset(0, 12),
+          ),
+          const BoxShadow(
+            color: colortheme.lightPurple,
+            offset: Offset(12, 0),
+          ),
+          const BoxShadow(
+            color: colortheme.lightPurple,
+            offset: Offset(-12, 0),
+          ),
+        ],
+      ),
+      child: MaterialButton(
+        // ignore: curly_braces_in_flow_control_structures
+        onPressed: onpress,
+        // ignore: curly_braces_in_flow_control_structures
+        child: Text(text,
+            style: const TextStyle(
+              fontSize: 20,
+              color: colortheme.white,
+            )),
+      ),
+    );
