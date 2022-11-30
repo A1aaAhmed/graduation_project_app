dc9575c1a7b7c1149367db6d96240ffe6ea92b61
Rana-Tarawi <ranatarawi45@gmail.com>
2022-11-30 05:28:08 +0200

    third commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,65 +23,52 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.3,
+          child: Stack(children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: BoxDecoration(
+                  color: colortheme.lightPurple,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft:
+                        Radius.circular(MediaQuery.of(context).size.width * .1),
+                    bottomRight:
+                        Radius.circular(MediaQuery.of(context).size.width * .1),
+                  )),
+              child: Stack(children: [
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Let\'s',
+                            style: TextStyle(
+                                color: colortheme.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                        Text('Book Your\nNext Trip ',
+                            style: TextStyle(
+                                color: colortheme.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold)),
+                      ]),
+                ),
+              ]),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.22,
+              left: MediaQuery.of(context).size.width * 0.05,
+              child: Container(
+                width: MediaQuery.of(context).size.width * .9,
+                height: MediaQuery.of(context).size.height * 0.65,
                 decoration: BoxDecoration(
-                    color: colortheme.lightPurple,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(
-                          MediaQuery.of(context).size.width * .1),
-                      bottomRight: Radius.circular(
-                          MediaQuery.of(context).size.width * .1),
+                    color: colortheme.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(MediaQuery.of(context).size.width * .05),
                     )),
-                child: Stack(children: [
-                  Padding(
-                    padding: const EdgeInsets.all(25),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Let\'s',
-                            style: TextStyle(
-                                color: colortheme.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Book Your ',
-                            style: TextStyle(
-                                color: colortheme.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            'Next Trip',
-                            style: TextStyle(
-                                color: colortheme.white,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ]),
-                  )
-                ]),
-              ),
-              Positioned(
-                top: MediaQuery.of(context).size.height * 0.22,
-                left: MediaQuery.of(context).size.width * 0.05,
-                child: Container(
-                  width: MediaQuery.of(context).size.width * .9,
-                  height: MediaQuery.of(context).size.height * 0.65,
-                  decoration: BoxDecoration(
-                      color: colortheme.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(
-                            MediaQuery.of(context).size.width * .05),
-                      )),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         TextFormField(
@@ -128,13 +115,11 @@ class _HomeScreenState extends State<HomeScreen> {
                             onpress: () {},
                             width: MediaQuery.of(context).size.width * 0.5,
                             height: 60),
-                      ],
-                    ),
-                  ),
+                      ]),
                 ),
               ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
