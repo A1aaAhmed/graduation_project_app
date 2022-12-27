33275a53d3233caaa99912780b378807b8f4e7d8
Rana-Tarawi <ranatarawi45@gmail.com>
2022-12-27 02:41:38 +0200

    rana2

--- lib/modules/welcome_screen.dart
+++ lib/modules/welcome_screen.dart
@@ -8,189 +8,174 @@ class ApisScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height:MediaQuery.of(context).size.height,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    repeat: ImageRepeat.noRepeat,
-                    image: AssetImage('images/train.jpg'),
-                    fit: BoxFit.cover),
-
-              ),
-              child: Container(
-                margin:const EdgeInsetsDirectional.only(top: 60,),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/train.jpg'),
-                    fit: BoxFit.cover,),
-
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              repeat: ImageRepeat.noRepeat,
+              image: AssetImage('images/train.jpg'),
+              fit: BoxFit.cover),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(
+                  35,
                 ),
-                //color: Colors.teal[300],
-
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: SingleChildScrollView(
-                    child: Column(
-                        children: [
-                          Container(
-                            decoration:BoxDecoration(
-                              borderRadius: BorderRadius.circular(35,),
-                              color: Colors.white.withOpacity(0.6),
-                            ) ,
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            padding: const EdgeInsetsDirectional.all(20),
-                            child: Column(
-                                children: const [
-                                  Text(
-                                    'Find Your best',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 23,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  Text('Timing and tickets',
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 23,
-                                      color: Colors.black,
-                                    ),
-                                  ),
-                                  SizedBox(
-                                    height: 20,
-                                  ),
-                                  Text('Booking your tickets online with your',
-
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 10,
-                                      color: Colors.black,
-
-                                    ),
-                                  ),
-                                  Text('best searching preferences all over EGYPT',
-
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 10,
-                                      color: Colors.black,
-
-                                    ),
-                                  ),
-
-                                ]),),
-
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                  5,
-                                ),
-                                color: Colors.blue[800]),
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            width: double.infinity,
-                            // color: Colors.blueAccent,
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.facebook,
-                                  color: Colors.white,
-                                ),
-                                MaterialButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                    'Continue with Facebook',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w300),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                  5,
-                                ),
-                                color: Colors.red),
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            width: double.infinity,
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.g_mobiledata,
-                                  color: Colors.white,
-                                ),
-                                MaterialButton(
-                                  onPressed: () {},
-                                  child: const Text(
-                                    'Continue with Google',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w300),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(
-                                  5,
-                                ),
-                                color: Colors.indigo[400]),
-                            clipBehavior: Clip.antiAliasWithSaveLayer,
-                            width: double.infinity,
-                            height: 50,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                const Icon(
-                                  Icons.email,
-                                  color: Colors.white,
-                                ),
-                                MaterialButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => loginSreen()),
-                                    );
-                                  },
-                                  child: const Text(
-                                    'log in with email',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w300),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-
-                        ]),
-                  ),),),
-            ) ));
+                color: Colors.white.withOpacity(0.7),
+              ),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              padding: const EdgeInsetsDirectional.all(20),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'Find Your best',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'Timing and tickets',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Booking your tickets online with your',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      'best searching preferences all over EGYPT',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 13,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ]),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
+                  color: Colors.blue[800]),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              width: double.infinity,
+              // color: Colors.blueAccent,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.facebook,
+                    color: Colors.white,
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Continue with Facebook',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
+                  color: Colors.red),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.g_mobiledata,
+                    color: Colors.white,
+                  ),
+                  MaterialButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Continue with Google',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    5,
+                  ),
+                  color: Colors.indigo[400]),
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              width: double.infinity,
+              height: 50,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.email,
+                    color: Colors.white,
+                  ),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => loginSreen()),
+                      );
+                    },
+                    child: const Text(
+                      'log in with email',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ]),
+        ),
+      ),
+    ));
   }
 }
--- lib/shared/variables.dart
+++ lib/shared/variables.dart
@@ -5,12 +5,12 @@ TextEditingController to = TextEditingController();
 TextEditingController depart = TextEditingController();
 int travellers = 1;
 List<Map> db = [
-  {'from': 'korea','to':'Aswan' ,'date':'2022-12-14 23:17:54.652181','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,k,kkkkkkkkkkkkkkkkkkk'},
-  {'from': 'Alexanddriaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa','to':'Aswan' ,'date':'2025-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
-  {'from': 'Alexanddria','to':'Aswannnnnnnnnnnnnnnnnnnnnnnnn' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
-  {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa','dur':'1hr-50min','seat':'A1,A2'},
+  {'from': 'Alexandria','to':'Aswan' ,'date':'2022-12-14 23:17:54.652181','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
+  {'from': 'Alexanddria','to':'Aswan' ,'date':'2025-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
   {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
-  {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250000000000000000000000000000000000','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
+  {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
+  {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
+  {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
   {'from': 'Alexanddria','to':'Aswan' ,'date':'2020-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
   {'from': 'Alexanddria','to':'Aswan' ,'date':'2000-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
   {'from': 'Alexanddria','to':'Aswan' ,'date':'2000-01-02 07:12:50.000','price':'250','gate':'A','dur':'1hr-50min','seat':'A1,A2'},
