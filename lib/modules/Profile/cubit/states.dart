14907be3f8e4c0a783e9802f1d9cad67552783b1
mahahassan25 <mh1733696@gmail.com>
2023-02-26 00:48:36 +0200

    lots of changes Done

---
+++ lib/modules/Profile/cubit/states.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class profileStates {}
+class profileInitialState extends profileStates {}
+class profilePickedImageProfileSucessState extends profileStates{}
+class profilePickedImageProfileErrortate extends profileStates{}
+class profileSetNameState extends profileStates{}
+class profileSetEmailState extends profileStates{}
+class profileSetPhoneState extends profileStates{}
