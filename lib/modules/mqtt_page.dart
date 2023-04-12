d349e86de5140660dacb0e831fb2c2ca2a212aa6
Rana-Tarawi <ranatarawi45@gmail.com>
2023-04-12 19:28:50 +0200

    commit1001

---
+++ lib/modules/mqtt_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class mqttPage extends StatefulWidget {
+  const mqttPage({super.key});
+
+  @override
+  State<mqttPage> createState() => _mqttPageState();
+}
+
+class _mqttPageState extends State<mqttPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
